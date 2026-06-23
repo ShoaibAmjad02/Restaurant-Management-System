f44f1e3586ee5c33241a9a327dc9a601ae3abc11
ShoaibAmjad02 <shoaibamjaddev@gmail.com>
2026-06-23 21:28:05 +0500

    Add PyMySQL dependency

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,15 @@ from .base import env  # noqa: F401
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[
+        "restaurant-management-system-production-3bdc.up.railway.app",
+        ".up.railway.app",
+        "localhost",
+        "127.0.0.1",
+    ],
+)
 
 DEBUG = False
 
